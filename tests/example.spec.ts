678fcff26a37c6a1edd99b3d0409926712b47ba1
rgsuhas <rgsuhas6364@gmail.com>
2025-08-07 18:11:08 +0530

    Add Playwright test report and update example test cases

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -11,8 +11,8 @@ test('get started link', async ({ page }) => {
   await page.goto('/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'Get Started Now' }).click();
 
   // Expects page to have a heading with the name of the current URL.
-  await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Create an account' })).toBeVisible();
 });
